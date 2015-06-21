81e5ffd8e41a445cabc95f188bbc548e5598083c
bboc <bernhard.bockelbrink@gmail.com>
2015-06-22 00:42:02 +0200

    processing metadata

--- metadata.py
+++ metadata.py
@@ -1,4 +1,46 @@
 # -*- coding: utf-8 -*-
 
+def read_metadata(source_file):
+    """Read metadata from source file.
 
+    Return a dictionary with the metadata, set the file seek pointer to 
+    the first character after the metadata.
+    """
+    # import pdb
+    # pdb.set_trace()
+    metadata = {}
+    first = True
+    while True:
+        line = source_file.readline().strip()
+        if first:
+            first = False
+            if line == '':
+                # no metadata
+                # TODO: change seek 
+                break
+            elif line == '---':
+                # begin metadata
+                continue
+        else:
+            if line in ['', '---', '...']:
+                # end of metadata
+                break
+
+        # extract key
+        pos = line.find(':')
+        key = line[:pos].replace(' ', '').lower()
+        value = line[pos+1:]
+        metadata[key] = value
+    return metadata
+
+
+if __name__ == '__main__':
+    from textwrap import dedent
+    from StringIO import StringIO
+    source = StringIO(dedent("""---
+        foo:bar:baz
+        ...
+        nometa: this
+        """))
+    metadata = read_metadata(source)
 
--- metadata_tests.py
+++ metadata_tests.py
@@ -8,75 +8,118 @@ from textwrap import dedent
 
 from StringIO import StringIO
 
-# TODO: multiline metadata
+from metadata import read_metadata
 
-class MetadataTests(unittest.TestCase):
+# TODO: multiline metadata
+# TODO: check file seek pointer in each section test
+
+
+class MetadataSectionTests(unittest.TestCase):
 
     def test_prefix_dashes(self):
         """There can optionally be a --- on the line before the metadata."""
 
-        metadata = dedent("""---
-            foo: bar:baz
+        source = StringIO(dedent("""\
+            ---
+            foo:bar:baz
 
             nometa: this
-        """)
+        """))
+        metadata = read_metadata(source)
+        self.assertDictContainsSubset(
+            {'foo': 'bar:baz'}, metadata, repr(metadata))
+        self.assertFalse(metadata.has_key('nometa'))
 
     def test_followed_by_dashes(self):
         """There can optionally be a --- on the line after the metadata."""
 
-        metadata = dedent("""foo: bar:baz
+        source = StringIO(dedent("""\
+            foo:bar:baz
             ---
             nometa: this
-            """)
+            """))
+        metadata = read_metadata(source)
+        self.assertDictContainsSubset(
+            {'foo': 'bar:baz'}, metadata, repr(metadata))
+        self.assertFalse(metadata.has_key('nometa'))
 
     def test_followed_by_dots(self):
-        """The line after the metadata can also be ...."""
+        """The line after the metadata can also be ..."""
 
-        metadata = dedent("""foo: bar:baz
-            ....
+        source = StringIO(dedent("""\
+            foo:bar:baz
+            ...
             nometa: this
-            """)
-
-    def test_no_blank_line_before(self):
-        """metadata must begin at the very top of the document, no blank lines can precede it."""
-
-        metadata = dedent("""
-            nometa: this
-            """)
+            """))
+        metadata = read_metadata(source)
+        self.assertDictContainsSubset(
+            {'foo': 'bar:baz'}, metadata, repr(metadata))
+        self.assertFalse(metadata.has_key('nometa'))
 
     def test_(self):
         """after the metadata is finished, a blank line triggers the beginning of the rest of the document."""
 
-        metadata = dedent("""
-        """)
+        source = StringIO(dedent("""\
+            foo:bar:baz
 
+            nometa: this
+        """))
+        metadata = read_metadata(source)
+        self.assertFalse(metadata.has_key('nometa'))
+
+    def test_no_blank_line_before(self):
+        """metadata must begin at the very top of the document, no blank lines can precede it."""
+
+        source = StringIO(dedent("""\
+
+            nometa: this
+            """))
+        metadata = read_metadata(source)
+        self.assertFalse(metadata.has_key('nometa'))
+
+
+class MetadataKeyValueTests(unittest.TestCase):
 
     def test_(self):
         """metadata key must begin at the beginning of the line"""
 
-        metadata = dedent("""
-        """)
+        source = StringIO(dedent("""\
+
+        """))
+        metadata = read_metadata(source)
 
     def test_(self):
         """metadata key starts with an ASCII letter or a number"""
 
-        metadata = dedent("""
-        """)
+        source = StringIO(dedent("""\
+        """))
+        metadata = read_metadata(source)
 
     def test_(self):
         """end of the metadata key is specified with a colon (‘:’)"""
 
-        metadata = dedent("""
-        """)
+        source = StringIO(dedent("""\
+        """))
+        metadata = read_metadata(source)
 
     def test_(self):
         """after the colon comes the metadata value, which can consist of pretty much any characters (including new lines)."""
 
-        metadata = dedent("""
-        """)
+        source = StringIO(dedent("""\
+        """))
+        metadata = read_metadata(source)
 
     def test_(self):
         """Metadata keys are case insensitive and stripped of all spaces during processing."""
 
-        metadata = dedent("""
-        """)
+        source = StringIO(dedent("""\
+            this is one key:first
+            tHis IsAno th   ER:second
+
+            nometa: this
+        """))
+        metadata = read_metadata(source)
+        self.assertDictContainsSubset(
+            {'thisisonekey': 'first',
+             'thisisanother': 'second'},
+            metadata, repr(metadata))
