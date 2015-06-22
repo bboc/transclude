e9bfce9afdcec24e0de838fb6f28fe001cada4cd
bboc <bernhard.bockelbrink@gmail.com>
2015-06-22 17:16:54 +0200

    renamed commandline script

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-print find_packages()
-
 setup(
     name = "transclude",
     packages = find_packages(),
 
     entry_points = {
         'console_scripts': [
-            'transclude = transclude:main',
+            'mmd-transclude = transclude:main',
         ],
     }
 
